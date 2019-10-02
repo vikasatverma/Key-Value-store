3f906f9832ebcbd160723b9357b171d60c469f48
Vikas Verma <33944095+vikasatverma@users.noreply.github.com>
2019-10-02 21:59:49 +0530

    Added CmakeList.txt
    Signed-off-by: VIKAS VERMA <vikasv@cse.iitb.ac.in>

--- KVServer.cpp
+++ KVServer.cpp
@@ -1,4 +1,4 @@
-
+#include "header.hpp"
 
 int main() {
     return 0;
--- header.hpp
+++ header.hpp
@@ -1,6 +1,4 @@
-//
-// Created by vikas on 02/10/19.
-//
+
 
 #ifndef KEY_VALUE_STORE_HEADER_HPP
 #define KEY_VALUE_STORE_HEADER_HPP
