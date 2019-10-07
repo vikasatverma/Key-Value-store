7b3e7e65586f2f8f16a5c44db13b58125607a047
VIKAS VERMA <vikasv@cse.iitb.ac.in>
2019-10-07 13:10:55 +0530

    Running server infinitely
    Signed-off-by: VIKAS VERMA <vikasv@cse.iitb.ac.in>

--- KVClient.cpp
+++ KVClient.cpp
@@ -1,70 +1,73 @@
+#include "header.hpp"
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <bits/stdc++.h>
-#include <string.h>
-#define PORT 8080 
+int checkLenght(const std::string &key, const std::string &value = " ") {
+    if (key.size() > max_key_lenght) {
+        cout << "Oversized key";
+        exit(-1);
+    }
+    if (value.size() > max_value_lenght) {
+        cout << "Oversized value";
+        exit(-1);
+    }
 
-using namespace std;
+}
 
-
-
-int main(){
-
-	cout<<"enter the request type"<<"\n";
-	cout<<"GET, PUT OR DEL\n";
-	string request_type;
-	cin>>request_type;
-	//cout<<"hello\n";
-	char buffer[1024] = {0};
-	int valread;
-	string key;
-	string value;
-
-	string request;
-
-	if(request_type == "GET"){
-		cin>>key;
-		request = request_type+ " " + key;
-	}
-	else if(request_type == "PUT"){
-		cin>>key;
-		cin>>value;
-		request = request_type+ " " + key + value;
-	}else if(request_type == "DEL"){
-		cin>>key;
-		request = request_type+ " " + key;	
-	}
-	//cout<<"hello\n";
-	//client connect on this
-	struct sockaddr_in serv_addr;
-	memset(&serv_addr, 0, sizeof(struct addrinfo));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-
-	//creating a socket 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	cout<<sockfd<<"\n";
-    if (sockfd < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-
-	connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+//TODO: Increase capacity of request string to store 256KB
 
 
-		cout<<request<<"\n";
-		cout<<"###################################### SENDING DATA #############################################\n";
-		send(sockfd , request.c_str() , request.size(), 0);
-		
+int main() {
 
-		valread = read(sockfd , buffer, 1024);
-		buffer[valread] = '\0';
-		cout<<buffer<<"\n";
+    cout << "enter the request type" << "\n";
+    cout << "GET, PUT OR DEL\n";
+    std::string request_type;
+    getline(cin, request_type);
+    //cout<<"hello\n";
+    char buffer[max_buffer_size] = {0};
+    int valread;
+    std::string key;
+    std::string value;
+    std::string request;
+
+    if (request_type == "GET") {
+        getline(cin, key);
+        request = request_type + delimiter + key;
+    } else if (request_type == "PUT") {
+        getline(cin, key);
+        getline(cin, value);
+        checkLenght(key, value);
+        request = request_type + delimiter + key + delimiter + value;
+    } else //if (request_type == "DEL")
+    {
+        getline(cin, key);
+        request = request_type + delimiter + key;
+    }
+
+    //client connect on this
+    struct sockaddr_in serv_addr = {AF_INET, htons(PORT)};
+    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    //creating a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    cout << sockfd << "\n";
+    if (sockfd < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+
+    cout << request << "\n";
+    cout << "###################################### SENDING DATA #############################################\n";
+
+    send(sockfd, request.c_str(), request.size(), 0);
+
+
+    valread = read(sockfd, buffer, max_buffer_size);
+    buffer[valread] = '\0';
+    cout << buffer << "\n";
 
 
 }
--- header.hpp
+++ header.hpp
@@ -1,6 +1,22 @@
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 
-#ifndef KEY_VALUE_STORE_HEADER_HPP
-#define KEY_VALUE_STORE_HEADER_HPP
+#define PORT 8080
+#define True 1
+#define False 0
+#define delimiter "_||_"
+#define max_key_lenght 256
+#define max_value_lenght (256*1024)
+#define max_buffer_size (256+256*1024+4*2)
 
-#endif //KEY_VALUE_STORE_HEADER_HPP
+using std::cout;
+using std::cin;
+
