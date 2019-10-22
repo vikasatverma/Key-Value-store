787c4e24ec7ec9c1ad810b49b17d0cbb074e078d
bansalapaar <bansalapaar99@gmail.com>
2019-10-23 00:10:07 +0530

    Update KVClient.cpp

--- KVClient.cpp
+++ KVClient.cpp
@@ -1,42 +1,51 @@
-#include<iostream>
-#include"header.hpp"
-using namespace std;
+#include "header.hpp"
+
 // to convert the plain text to xml format
-std::string plaintoxml(std::string msg_type,std::string key,std::string value ="") 
-{
+std::string toxml(std::string msg_type, std::string key, std::string value = "") {
 
-std::string request ="<?xml version='1.0' encoding='UTF-8'?>\n";
+    std::string request = "<?xml version='1.0' encoding='UTF-8'?>\n";
 
-if(msg_type=="GET")
-{
- msg_type="<KVMessage type='getreq'>\n";
- key="<Key>"+key+"</Key>\n";
+    if (msg_type == "GET") {
+        msg_type = "<KVMessage type='getreq'>\n";
+        key = "<Key>" + key + "</Key>\n";
+    }
+    if (msg_type == "PUT") {
+
+        msg_type = "<KVMessage type='putreq'>\n";
+        key = "<key>" + key + "</Key>\n";
+        value = "<Value>" + value + "</Value>\n";
+        key = key + value;
+
+    }
+    if (msg_type == "DEL") {
+
+        msg_type = "<KVMessage type='delreq'>\n";
+        key = "<Key>" + key + "</Key>\n";
+    }
+
+    request = request + msg_type + key + "</KVMessage>\n";
+
+    return request;
 }
-if(msg_type=="PUT")
-{
 
- msg_type="<KVMessage type='putreq'>\n";
- key= "<key>"+key+"</Key>\n";
- value="<Value>"+value+"</Value>\n";
- key=key+value;
-
-}
-if(msg_type=="DEL")
-{
-
- msg_type="<KVMessage type='delreq'>\n";
- key="<Key>"+key+"</Key>\n";
-}
-
-request= request+msg_type+key+"</KVMessage>\n";
-
-return request;
-}
 //to get back xml format to plain text
-std::string xmltoplain(std::string request)
-{
+std::string xmltoplain(std::string str) {
 
-return request;
+    int i = 0;
+    std::string plain_text;
+    if (str[64] == 'M') {
+        for (i = 72; str[i] != '<'; i++)
+            plain_text += str[i];
+    } else {
+        for (i = 68; str[i] != '<'; i++)
+            plain_text += str[i];
+        plain_text += " ";
+        int j = i + 14;
+        for (; str[j] != '<'; j++)
+            plain_text += str[j];
+
+    }
+    return plain_text;
 }
 
 int checkLenght(const std::string &key, const std::string &value = " ") {
@@ -52,79 +61,93 @@ int checkLenght(const std::string &key, const std::string &value = " ") {
 }
 
 //TODO: Increase capacity of request string to store 256KB
-//TODO: Accept input from the file batchRun.txt
-
 
 int main() {
-
-    cout << "enter the request type" << "\n";
-    cout << "GET, PUT OR DEL\n";
+    std::ifstream infile("batchRun.txt");
     std::string request_type;
-    getline(cin, request_type);
-    //cout<<"hello\n";
-    char buffer[max_buffer_size] = {0};
-    int valread;
     std::string key;
     std::string value;
-    std::string request;
-   
-    if (request_type == "GET") {
-        getline(cin, key);
-        checkLenght(key, value);
-        //if(key.length()<256)
-        //	key.resize(256);
-        	
-        request=plaintoxml(request_type,key,value);
-	
-    } else if (request_type == "PUT") {
-        getline(cin, key);
-        getline(cin, value);
-        checkLenght(key, value);
-        //if(key.length()<256)
-		//key.resize(256,0);
-	request=plaintoxml(request_type,key,value);
-		
-    } 
-	else if (request_type == "DEL")
-    {
-        getline(cin, key);
-        checkLenght(key, value);
-        //if(key.length()<256)
-        //	key.resize(256);
-        request=plaintoxml(request_type,key);
-    } else {
-        cout << "Unknown Error: Undefined request type";
-        exit(-1);
+    char buffer1[max_buffer_size] = {0};
+    int valread;
+    std::string finalRequest;
+
+    for (std::string line; getline(infile, line);) {
+        finalRequest = "";
+        std::vector<std::string> request = split(line.c_str(), ',');
+        if (debugger_mode) {
+            cout << request[0] << "\t" << request[1] << "\t";
+        }
+        request_type = request[0];
+        key = request[1];
+//        finalRequest.append(request[0]).append(delimiter).append(request[1]);
+        if (request[0] == "PUT") {
+            if (debugger_mode) {
+                cout << request[2];
+            }
+            checkLenght(request[1], request[2]);
+            value = request[2];
+//            finalRequest.append(delimiter).append(request[2]);
+        } else if (request[0] != "GET" && request[0] != "DEL") {
+            cout << "Unknown Error: Undefined finalRequest type";
+            exit(-1);
+        }
+
+        if (debugger_mode) {
+            cout << "\n";
+        }
+        finalRequest = toxml(request_type, key, value);
+        if (debugger_mode) {
+            cout << finalRequest << "\n";
+            cout << "###################################### "
+                    "SENDING DATA "
+                    "######################################\n";
+        }
+
+        struct sockaddr_in serv_addr = {AF_INET, htons(PORT)};
+        memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+
+        //creating a socket
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (debugger_mode) {
+            cout << sockfd << "\n";
+        }
+        if (sockfd < 0) {
+            printf("\n Socket creation error \n");
+            exit(-1);
+        }
+
+        connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+        send(sockfd, finalRequest.c_str(), finalRequest.size(), 0);
+
+        valread = read(sockfd, buffer1, max_buffer_size);
+        buffer1[valread] = '\0';
+        cout<<buffer1<<std::endl;
+
+        std::string buffer;
+        for (int i = 0; i < valread; i++) {
+            buffer += (buffer1[i]);
+        }
+
+
+        std::string buffer2 = xmltoplain(buffer);
+
+        char chararr_of_buffer[buffer2.length() + 1];
+        strcpy(chararr_of_buffer, buffer2.c_str());
+        if (debugger_mode) {
+            cout << chararr_of_buffer << "\n";
+        }
+        FILE *fp = fopen("response.txt", "a");
+        if (!fp) {
+            return -errno;
+        }
+        fprintf(fp, "%s", chararr_of_buffer);
+        fprintf(fp, "\n");
+
+        fclose(fp);
+
     }
 
-    //cout<<request;
-    struct sockaddr_in serv_addr = {AF_INET, htons(PORT)};
-    memset(&serv_addr, 0, sizeof(struct sockaddr_in));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    //creating a socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (debugger_mode) {
-        cout << sockfd << "\n";
-    }
-    if (sockfd < 0) {
-        printf("\n Socket creation error \n");
-        exit(-1);
-    }
-
-    connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-
-    if (debugger_mode) {
-    cout << request << "\n";
-    cout << "###################################### SENDING DATA #############################################\n";
-    }
-  
-    send(sockfd, request.c_str(), request.size(), 0);
-
-
-    valread = read(sockfd, buffer, max_buffer_size);
-    buffer[valread] = '\0';
-    cout << buffer << "\n";
-  
 }
